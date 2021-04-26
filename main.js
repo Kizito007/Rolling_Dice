b4c7aa0c216e81e81066c1a1a436026fd9d7b850
Kizito007 <nwakakelechi49@gmail.com>
2021-04-26 21:23:25 +0100

    Started dice logic

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-//Pick a random number from 1 to 6 and display it on the screen
+//Pick a random number from 1 to 6 for each of the 3 dies
 let rand1;
+let rand2;
+let rand3;
+let result;
 
-const outcome1 = () => {
+const outcome = () => {
     rand1 = Math.floor(Math.random() * 6) + 1
-    console.log(rand1)
+    rand2 = Math.floor(Math.random() * 6) + 1
+    rand3 = Math.floor(Math.random() * 6) + 1
+    console.log(rand1, rand2, rand3)
+    result = rand1 + rand2 + rand3;
+    //Display Result 
+    document.getElementById("result").innerHTML = result;
 }
+
+//Assign from die one to die six to a digit from its random outcome
